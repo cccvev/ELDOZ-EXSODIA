a898a5aa6884df6d6e8357ec09ccbfd1b41a0973
cccvev <75307540+cccvev@users.noreply.github.com>
2020-12-01 12:29:10 +0200

    Add files via upload

---
+++ ELDOWZ-EXSODIA.py
@@ -0,0 +1,14 @@
+#THIS TOOL BY REDVIROUS
+import os
+os.system("clear")
+from N4Tools.Design import Text
+T=Text()
+about='''هذه اداه من صنع الدوز اكسوديا
+ABOUT: python lang
+coded ELDOZ EXSODIA
+
+'''
+arabic=input(T.arabic(about))
+print ("")
+print ("")
+print (T.arabic(arabic))
